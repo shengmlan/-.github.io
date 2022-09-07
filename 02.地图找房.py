6baf124787f678d660870e9b066135d935f5007b
shengmlan <47288721+shengmlan@users.noreply.github.com>
2022-09-07 15:16:32 +0800

    Add files via upload

---
+++ 02.地图找房.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+import time
+#网址
+url="https://bj.58.com/pinpaigongyu/"
+#res_text =requests.get(url).text
+#with open("zhaofang.html",'w',encoding="utf-8") as fp:
+   # fp.write(res_text)
+
